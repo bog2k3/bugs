e23e2b0dc901c6c495053b6f190927fbeb1fdf5c
bogdan ionita <bog2k3@gmail.com>
2014-11-13 17:37:52 +0200

    update

---
+++ bugs/input/operations/OperationSpring.cpp
@@ -0,0 +1,18 @@
+/*
+ * OperationSpring.cpp
+ *
+ *  Created on: Nov 13, 2014
+ *      Author: bogdan
+ */
+
+#include "OperationSpring.h"
+
+OperationSpring::OperationSpring() {
+	// TODO Auto-generated constructor stub
+
+}
+
+OperationSpring::~OperationSpring() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ bugs/input/operations/OperationSpring.h
@@ -0,0 +1,33 @@
+/*
+ * OperationSpring.h
+ *
+ *  Created on: Nov 13, 2014
+ *      Author: bogdan
+ */
+
+#ifndef INPUT_OPERATIONS_OPERATIONSPRING_H_
+#define INPUT_OPERATIONS_OPERATIONSPRING_H_
+
+#include "IOperation.h"
+
+class MouseObject;
+
+class OperationSpring: public IOperation {
+public:
+	OperationSpring(MouseObject* mObj, InputEvent::MOUSE_BUTTON boundButton);
+	virtual ~OperationSpring();
+
+	virtual void enter(const OperationContext* pContext);
+	virtual void leave();
+	virtual void activate();
+	virtual void deactivate();
+	virtual void handleInputEvent(InputEvent& ev);
+	virtual void update(float dt);
+
+protected:
+	MouseObject* mouse;
+	const OperationContext* pContext;
+	InputEvent::MOUSE_BUTTON boundButton;
+};
+
+#endif /* INPUT_OPERATIONS_OPERATIONSPRING_H_ */
