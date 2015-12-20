77b375191db26c48a1022ee4f375074c1dc139f5
Bogdan I <bog2k3@gmail.com>
2015-12-20 19:40:52 +0200

    makefiles

--- bugs/Debug/entities/subdir.mk
+++ bugs/Debug/entities/subdir.mk
@@ -26,7 +26,7 @@ CPP_DEPS += \
 entities/%.o: ../entities/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -std=c++0x -DGLM_FORCE_RADIANS -DDEBUG -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++0x -DGLM_FORCE_RADIANS -DDEBUG -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
