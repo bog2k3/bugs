8fecd327a74b6c5097aa8df5061b45e6c53b7296
bogdan ionita <bog2k3@gmail.com>
2015-12-27 00:12:10 +0200

    dmalloc debug

--- bugs/Debug/body-parts/subdir.mk
+++ bugs/Debug/body-parts/subdir.mk
@@ -41,7 +41,7 @@ CPP_DEPS += \
 body-parts/%.o: ../body-parts/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: Cross G++ Compiler'
-	g++ -std=c++0x -DGLM_FORCE_RADIANS -DDEBUG -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	g++ -std=c++0x -DGLM_FORCE_RADIANS -DDMALLOC -DDEBUG_DMALLOC -DDEBUG -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
