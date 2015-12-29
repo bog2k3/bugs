58d661b0e67b3dcb61e1a9d573a6c348c8299fbf
Bogdan I <bog2k3@gmail.com>
2015-12-29 19:47:01 +0200

    unit test proj

---
+++ bugs-tests/main.cpp
@@ -0,0 +1,19 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Dec 29, 2015
+ *      Author: bog
+ */
+
+#include <easyunit/testharness.h>
+
+using namespace easyunit;
+
+int main()
+{
+
+  TestRegistry::runAndPrint();
+
+}
+
+
