c7a7bbfe304796bbc4d84361be86f0e0315539c7
Bogdan I <bog2k3@gmail.com>
2016-06-18 12:51:31 +0300

    multi-threaded update; MUST FIX concurency with Box2D

--- bugs/utils/UpdateList.h
+++ bugs/utils/UpdateList.h
@@ -12,6 +12,7 @@
 #include <vector>
 #include <algorithm>
 #include <iterator>
+#include <future>
 
 class UpdateList {
 public:
@@ -38,8 +39,23 @@ public:
 		}), list_.end());
 		pendingRemove_.clear();
 		// do update on current elements:
-		for (auto &w : list_)
-			w.update(dt);
+		// split list into 4 subsets and run them on separate threads
+		uint quarterSz = std::max((size_t)1, list_.size() / 4);
+		uint n1 = quarterSz;
+		uint n2 = n1 == list_.size() ? n1 : n1 + quarterSz;
+		uint n3 = n2 == list_.size() ? n2 : n2 + quarterSz;
+		auto updateInterval = [this, dt] (uint nB, uint nE) {
+			for (uint i=nB; i<nE; i++)
+				list_[i].update(dt);
+		};
+		auto t1 = std::async(std::launch::async, updateInterval, 0, n1);
+		auto t2 = std::async(std::launch::async, updateInterval, n1, n2);
+		auto t3 = std::async(std::launch::async, updateInterval, n2, n3);
+		auto t4 = std::async(std::launch::async, updateInterval, n3, list_.size());
+		t1.wait();
+		t2.wait();
+		t3.wait();
+		t4.wait();
 	}
 
 private:
